2624f023e8a14fb39651b83f13e935a01d61f3c8
FMate01 <fulopmate0@gmail.com>
2024-11-13 22:07:56 +0100

    logging added to main_server

--- main_server.py
+++ main_server.py
@@ -2,28 +2,32 @@ import socket
 import threading
 import time
 import datetime
+import logging
+from sys import stdout
 
-print("Welcome to Stray Host!")
+logger = logging.getLogger("app")
+logging.basicConfig(stream=stdout, encoding='utf-8', level=logging.DEBUG)
+
+logger.info("Welcome to Stray Host!")
 
 admins = ["PKB"]
 
-print(datetime.datetime.now())
-
-#select * from connection
+logger.info(datetime.datetime.now())
 
 with open('version', 'r') as k:
     VERSION = k.read()
-    print(f"Version : {VERSION}")
+    logger.info(f"Version : {VERSION}")
 
 HEADER = 64
 FORMAT = 'utf-8'
 
 
-print(" __  _____  ___    __    _    \n"
-"( (`  | |  | |_)  / /\  \ \_/ \n"
-"_)_)  |_|  |_| \ /_/--\  |_|  \n")
+logger.info("""
+ __  _____  ___    __    _    
+( (`  | |  | |_)  / /\  \ \_/ 
+_)_)  |_|  |_| \ /_/--\  |_|  """)
 
-print("Starting...")
+logger.info("Starting...")
 
 
 def handle_client(clientsocket, address):
@@ -33,64 +37,67 @@ def handle_client(clientsocket, address):
         with open('servers/server_list', 'r') as k:
             servers = k.read()
         if client == '0':
-            print("Client detected!")
+            logger.info("Client detected!")
             wait_time = 0.01
             var = "b'"
             var2 = "'"
-            if True:
-                with open('servers/server_list', 'r') as k:
-                    servers = k.read()
-                    print("Sending list...")
-                    clientsocket.send(servers.encode("utf-8"))
-                    time.sleep(0.1)
-                    server = clientsocket.recv(1024)
-                    print(server)
-                    server = str(server).replace("b'", "").replace("'", "")
-                with open(f'servers/server_configs/{server}', 'r') as k:
-                    print("start")
-                    ip = k.read()
-                    print(ip)
-                    clientsocket.send(ip.encode("utf-8"))
-                    print("sent ip")
-    except:
-        print("FATAL ERROR AT CLIENT HANDLING!")
+            with open('servers/server_list', 'r') as k:
+                servers = k.read()
+                logger.info("Sending list...")
+                clientsocket.send(servers.encode("utf-8"))
+                time.sleep(0.1)
+                server = clientsocket.recv(1024)
+                logger.info(server)
+                server = str(server).replace("b'", "").replace("'", "")
+            with open(f'servers/server_configs/{server}', 'r') as k:
+                logger.info("start")
+                ip = k.read()
+                logger.info(ip)
+                clientsocket.send(ip.encode("utf-8"))
+                logger.info("sent ip")
+    except Exception as e:
+        logger.info("FATAL ERROR AT CLIENT HANDLING!")
+        logger.info(e)
     else:
         try:
-            print("Server Detected!")
+            logger.info("Server Detected!")
             with open('servers/server_list', 'a') as a:
                 server_name = clientsocket.recv(1024)
+                logger.info(server_name)
                 server_name = str(server_name).replace(r"\n", "")
                 server_name = str(server_name).replace("b'", "").replace("'", "")
                 if server_name not in servers:
                     a.write(f"{server_name}\n")
             with open(f'servers/server_configs/{server_name}', 'w') as k:
                 addressdata = str(address)
+                logger.info(addressdata)
                 addressdata = addressdata.replace('(', '')
                 addressdata = addressdata.replace(')', '')
                 addressdata = addressdata.replace("'", "")
                 head, sep, tail = addressdata.partition(',')
                 k.write(head)
-        except:
-            print("FATAL ERROR AT Server Indexing!")
+        except Exception as error:
+            logger.info("FATAL ERROR AT Server Indexing!")
+            logger.info(f"{type(error).__name__}: {error}")
 
 
 def listen():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(None)
-    print('[STARTING] Binding sockets...')
+    logger.info('[STARTING] Binding sockets...')
 
     s.bind(('127.0.0.1', 5667))  # general interface
     s.listen(10)
-    print('[STARTING] Started!')
-    print('[LISTENING] Listening for maximum 10 users...')
+    logger.info('[STARTING] Started!')
+    logger.info('[LISTENING] Listening for maximum 10 users...')
     while True:  # might be a huge risk for the cloud
         clientsocket, address = s.accept()
-        print(f'[CLIENT] Got connection : {address}')
+        logger.info(f'[CLIENT] Got connection : {address}')
 
         t = threading.Thread(target=handle_client, args=(clientsocket, address))
         t.start()
 
 
-print('[STARTING] Starting...')
+logger.info('[STARTING] Starting...')
 listen()
 
