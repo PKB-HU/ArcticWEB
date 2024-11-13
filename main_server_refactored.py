f44b162b6e7dc7421b51aad5e29f27f9e5412c7a
FMate01 <fulopmate0@gmail.com>
2024-11-13 22:39:54 +0100

    Added fully working HUB-server with OOP

---
+++ main_server_refactored.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+import os
+import logging
+from sys import stdout
+
+logging.basicConfig(stream=stdout, encoding='utf-8', level=logging.DEBUG)
+
+class MainServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.logger = logging.getLogger("MainServer")
+        self.logger.info("""
+            __  _____  ___    __    _    
+            ( (`  | |  | |_)  / /\  \ \_/ 
+            _)_)  |_|  |_| \ /_/--\  |_|  """)
+        self.logger.info(f"Starting main server on {host}:{port}")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(None)
+
+    def start(self):
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(10)
+        self.logger.info("Waiting for connections!")
+        self.logger.warning("Maximum 10 concurrent users allowed!")
+        while True:
+            client_socket, client_address = self.socket.accept()
+            self.logger.info(f"Connection from {client_address[0]} established!")
+            threading.Thread(target=self.handle_client, args=(client_socket, client_address)).start()
+        
+    def handle_client(self, client_socket: socket.socket, client_address:socket.AddressInfo):
+        is_server = client_socket.recv(1).decode() == "1"
+        if is_server:
+            pass
+            # TODO implement
+        else:
+            # send back list of available servers
+            servers = os.listdir("servers/server_configs")
+            serverlist_message = "Available servers:\n"
+            for server in servers:
+                serverlist_message += f"\t{server}\n"
+            client_socket.send(serverlist_message.encode())
+            # receive server selection
+            selected_server = client_socket.recv(1024).decode()
+            # connect to selected server
+            with open(f"servers/server_configs/{selected_server}", "r") as server_config:
+                server_address = server_config.read()
+                client_socket.send(server_address.encode())
+            # My work here is done
+            client_socket.close()
+
+if __name__ == "__main__":
+    main_server = MainServer("127.0.0.1", 5667)
+    main_server.start()
